5e7f8eaa151bd5f696353998bf0f7178c57f9644
sebas <snarvaez.ieu2023@student.ie.edu>
2025-01-24 11:27:01 +0100

    First Commit

---
+++ Session 5/Session 5 Friday.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ")
+# print("Hello", name)
+# age = int(input("How old are you? " + name + "? "))
+# print("You were born in " + str(2024-age))
+
+# Simpler way
+print(f"Hello {name}")
+age2 = int(input(f"How old are you {name}? "))
+print(f"{name}, you were born in {2024-age2}")
+
+
