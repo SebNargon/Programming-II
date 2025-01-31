f77314e7cbee07014285108088f17ac155b3b6bb
sebas <snarvaez.ieu2023@student.ie.edu>
2025-01-31 11:24:03 +0100

    add string examples

---
+++ Session 7/simple_string.py
@@ -0,0 +1,9 @@
+s = "Hello"
+print(s)
+s2 = 'World'
+print(s2)
+s3 = ('And she said: "Hello!"')
+s4 = (""""Both single" and 'double'""")
+print("\"Both single\" and 'double'")
+print(s[2])
+print(s[-1])
